a2d235c2619811917cffae44caf158d14e65f3b7
mich1 <Mich.1999@hotmail.com>
2023-12-08 10:06:44 +0100

    live environment sat op

--- settings/init.php
+++ settings/init.php
@@ -9,10 +9,10 @@ if(CONFIG_LIVE == 0){
     $DB_USER = "root";
     $DB_PASS = "";
 }else{
-    $DB_SERVER = "";
-    $DB_NAME = "";
-    $DB_USER = "";
-    $DB_PASS = "";
+    $DB_SERVER = "mysql97.unoeuro.com";
+    $DB_NAME = "multimediemichael_dk_db";
+    $DB_USER = "multimediemichael_dk";
+    $DB_PASS = "rGxcfewpdhBgmARnEFtD";
 }
 
 $db = new db($DB_SERVER, $DB_NAME, $DB_USER, $DB_PASS);
