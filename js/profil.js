cf6b552929975225151eca5dbe9fcaa0f11d4877
mich1 <Mich.1999@hotmail.com>
2023-12-08 14:29:01 +0100

    profilliste lavet

---
+++ js/profil.js
@@ -0,0 +1,88 @@
+export default class Profilliste{
+
+    constructor() {
+        this.data ={
+            password: 'Unimatch'
+        }
+
+        this.rootElem = document.querySelector('.Profilliste');
+        this.filter = this.rootElem.querySelector('.filter')
+        this.items = this.rootElem.querySelector('.items')
+
+        this.afstandSearch = this.filter.querySelector('.afstandSearch')
+        this.forholdSearch = this.filter.querySelector('.forholdSearch')
+        this.alderSearch = this.filter.querySelector('.alderSearch')
+
+    }
+    async init(){
+
+        this.afstandSearch.addEventListener('input', () =>{
+            if (this.afstandSearch.value.length >= 3 ){
+                this.render()
+            }
+        });
+        this.forholdSearch.addEventListener('input', () =>{
+            if (this.forholdSearch.value.length >= 2 ){
+                this.render()
+            }
+        });
+
+        this.alderSearch.addEventListener('input', () =>{
+            if (this.alderSearch.value.length >= 2 ){
+                this.render()
+            }
+        });
+
+        await this.render();
+    }
+
+    async render(){
+        const data = await this.getData();
+        const row = document.createElement('div');
+        row.classList.add('row');
+        console.log(data)
+
+        for (const item of data){
+            const col  = document.createElement('div');
+            col.classList.add('col-sm-6','col-md-6', 'col-lg-4', 'col-xl-3');
+
+            col.innerHTML = `
+            <div class="card">
+            ${(item.MatchBillede) ? `<img src="uploads/${item.MatchBillede}" class="card-img-top" >` : "Ingen billede" }
+            
+            <div class="card-body">
+            <h5 class="card-title">${item.MatchNavn}</h5>
+            <p class="card-text">${item.MatchAfstand}</p>
+            <p class="card-text">${item.MatchAlder}</p>
+            <p class="card-text">${item.MatchForhold}</p>
+            <p class="card-text">${item.MatchBeskrivelse}</p>
+            <a href="Profilside.php?Matchid=${item.MatchId}" class="btn btn-primary text-white w-100">se profil</a>
+
+            </div>
+            
+             
+             </div> 
+            
+            `;
+            row.appendChild(col)
+        }
+
+        this.items.innerHTML= '';
+        this.items.appendChild(row)
+
+    }
+
+    async getData(){
+
+        this.data.afstandSearch = this.afstandSearch.value;
+        this.data.forholdSearch = this.forholdSearch.value;
+        this.data.alderSearch = this.alderSearch.value;
+
+        const  response = await fetch('api.php', {
+            method: "POST",
+            body: JSON.stringify(this.data)
+
+        })
+        return await response.json();
+    }
+};
--- profilListe.php
+++ profilListe.php
@@ -1,1 +1,111 @@
-<?php
+
+
+<!DOCTYPE html>
+<html lang="da">
+<head>
+    <meta charset="utf-8">
+
+    <title>profilliste </title>
+
+    <meta name="robots" content="All">
+    <meta name="author" content="Udgiver">
+    <meta name="copyright" content="Information om copyright">
+
+    <link href="css/styles.css" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v6.4.2/css/all.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Work+Sans:wght@300&display=swap" rel="stylesheet">
+
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+<body>
+
+<nav class="navbar navbar-expand-lg bg-primary text-white">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#"></a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="index.php"></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="profilListe.php">liste</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="profilside.php">profilside</a>
+                </li>
+
+
+            </ul>
+
+        </div>
+    </div>
+</nav>
+
+
+<div class="container">
+
+    <div class="Profilliste">
+        <div class="filter p-5">
+            <div class="row">
+                <div class="col-md-4 ">
+                    <input type="search" class="form-control afstandSearch" placeholder="søg og find en afstand">
+                </div>
+                <div class="col-md-4 ">
+                    <input type="search" class="form-control forholdSearch" placeholder="søg og find en forholdstype">
+                </div>
+                <div class="col-md-4 ">
+                    <input type="search" class="form-control alderSearch" placeholder="søg og find en alder">
+                </div>
+            </div>
+        </div>
+        <div class="items">
+
+        </div>
+
+    </div>
+</div>
+
+
+<footer>
+    <div class="container-fluid text-dark text-center bg-primary h-100">
+
+
+        <a class="btn btn-outline-dark btn-floating mb-2" href="" role="button">
+            <i class="fa-solid fa-house"></i>
+        </a>
+
+        <a class="btn btn-outline-dark btn-floating mb-2" href="#" role="button">
+            <i class="fa-solid fa-heart"></i>
+        </a>
+        <a class="btn btn-outline-dark btn-floating mb-2" href="#" role="button">
+            <i class="fa-solid fa-calendar-days"></i>
+        </a>
+        <a class="btn btn-outline-dark btn-floating mb-2" href="#" role="button">
+            <i class="fa-solid fa-comments"></i>
+        </a>
+        <a class="btn btn-outline-dark btn-floating mb-2" href="#" role="button">
+            <i class="fa-solid fa-user"></i>
+        </a>
+
+
+    </div>
+</footer>
+
+
+
+<script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+<script type="module">
+    import Profil from "./js/profil.js";
+    const profil = new Profil()
+    profil.init()
+</script>
+</body>
+</html>
+
