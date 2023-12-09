fd780bba296eba02e7c9e1b2266632ac730eec19
mich1 <Mich.1999@hotmail.com>
2023-12-09 20:47:19 +0100

    forside lavet

--- index.php
+++ index.php
@@ -13,39 +13,29 @@ require "settings/init.php";
 	<meta name="copyright" content="Information om copyright">
 
 	<link href="css/styles.css" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v6.4.2/css/all.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Work+Sans:wght@300&display=swap" rel="stylesheet">
 	
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 
-<body
+
+<div class="container col-sm-12 ">
+    <div class="row">
+        <img src="images/UMlogo.png" alt="">
+        <a href="profilListe.php" class="btn btn-light w-100 m-3">Log ind</a>
+        <a href="" class="btn btn-light w-100 m-3">opret bruger</a>
+
+    </div>
 </div>
 
 
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
-	<div class="container-fluid">
-		<a class="navbar-brand" href="#"></a>
-		<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-			<span class="navbar-toggler-icon"></span>
-		</button>
-		<div class="collapse navbar-collapse" id="navbarSupportedContent">
-			<ul class="navbar-nav me-auto mb-2 mb-lg-0">
-				<li class="nav-item">
-					<a class="nav-link active" aria-current="page" href="#"></a>
-				</li>
-				<li class="nav-item">
-					<a class="nav-link active" aria-current="page" href="#"></a>
-				</li>
-				<li class="nav-item">
-					<a class="nav-link active" aria-current="page" href="#"></a>
-				</li>
 
-
-			</ul>
-
-		</div>
-	</div>
-</nav>
 
 <script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
 </body>
+
+
 </html>
